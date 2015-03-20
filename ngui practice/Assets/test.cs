e0cca57da844509580b70c45253d05359257cf61
softwarekid <784810053@qq.com>
2015-03-20 18:04:54 +0800

    3/20 practice
    expriment scrollbar and scroll-view

---
+++ ngui practice/Assets/test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour 
+{
+    public void MakeItRed()
+    {
+        GetComponent<UIWidget>().color = Color.red;
+    }
+
+    public void MakeItGreen()
+    {
+        GetComponent<UIWidget>().color = Color.green;
+    }
+
+    public void MakeItBlue()
+    {
+        GetComponent<UIWidget>().color = Color.blue;
+    }
+}
